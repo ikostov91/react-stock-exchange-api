97d930d3d4b3f006b8b4f4f2d78440e6b8b761b4
DESKTOP-ROOTV37\Ivaylo Kostov <i.kostov91@gmail.com>
2023-05-22 11:42:08 +0300

    Initial commit

---
+++ Controllers/StockExchangeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockExchangeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockExchangeController : ControllerBase
+    {
+        public StockExchangeController() { }
+
+        [HttpGet]
+        [Route("companies/{databaseId}")]
+        public async Task<IActionResult> GetCompanies([FromRoute] string databaseId, [FromServices] IConfiguration configuration)
+        {
+            string url = $"https://data.nasdaq.com/api/v3/datasets/?database_code={databaseId}&api_key={configuration.GetValue<string>("ApiKey")}";
+            var result = await SendRequest(url);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("company-data/{databaseId}/{companyId}")]
+        public async Task<IActionResult> GetCompanyData([FromRoute] string companyId, [FromServices] IConfiguration configuration)
+        {
+            string url = $"https://data.nasdaq.com/api/v3/datasets/BSE/{companyId}/data.json?column_index=1&column_index=4&api_key={configuration.GetValue<string>("ApiKey")}";
+            var result = await SendRequest(url);
+
+            return Ok(result);
+        }
+
+        private async Task<string> SendRequest(string url)
+        {
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            requestMessage.Headers.Add("Accept", "application/json");
+
+            using var httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
+            var content = await response.Content.ReadAsStringAsync();
+            Response.Headers.Add("Content-Type", "application/json");
+
+            return content;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+namespace react_stock_exchange_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+            });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register and configure CORS
+            const string _policyName = "CorsPolicy";
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: _policyName,
+                    options =>
+                    {
+                        options// .WithOrigins(builder.Configuration.GetValue<string>("Origins"))
+                            .AllowAnyHeader()
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(_policyName);
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
